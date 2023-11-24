ae70800f482b8cda20879e05ca615bd10a4e9176
rodrigoFfreire <rodrigofreitasfreire@gmail.com>
2023-11-24 03:16:03 +0000

    fix: too big pieces are not even considered

--- Project1/src/main.rs
+++ Project1/src/main.rs
@@ -10,6 +10,16 @@ use solver::{
 };
 use std::{io, num::ParseIntError};
 
+macro_rules! check_fit {
+    ($piece_x:expr, $piece_y:expr, $sheet_x:expr, $sheet_y:expr) => {
+        $piece_x <= $sheet_x && $piece_y <= $sheet_y
+    };
+
+    (rotated, $piece_x:expr, $piece_y:expr, $sheet_x:expr, $sheet_y:expr) => {
+        $piece_x <= $sheet_y && $piece_y <= $sheet_x
+    };
+}
+
 
 fn parse_integer_tokens(amount: usize) -> Result<Vec<usize>, String> {
     let mut input = String::new();
@@ -34,13 +44,19 @@ fn parse_integer_tokens(amount: usize) -> Result<Vec<usize>, String> {
 fn main() {
     let sheet = parse_integer_tokens(PARSE_ARGS_SHEET_SIZE).expect(ERR_FAILED_PARSE);
 
-    let piece_amount = parse_integer_tokens(PARSE_ARGS_PIECE_AMOUNT).expect(ERR_FAILED_PARSE)[0];
+    let mut piece_amount = parse_integer_tokens(PARSE_ARGS_PIECE_AMOUNT).expect(ERR_FAILED_PARSE)[0];
 
     let mut order = Vec::with_capacity(piece_amount);
 
     for _ in 0..piece_amount {
         let piece_info = parse_integer_tokens(ARGS_PIECE).expect(ERR_FAILED_PARSE);
-        order.push(Piece::new(&piece_info));
+        if !check_fit!(piece_info[0], piece_info[1], sheet[0], sheet[1]) &&
+            !check_fit!(rotated, piece_info[0], piece_info[1], sheet[0], sheet[1]) {
+                piece_amount -= 1;
+            }
+        else {
+            order.push(Piece::new(&piece_info));
+        }
     }
 
     let result = solve_best_value(&order, piece_amount, sheet[DIM_X], sheet[DIM_Y]);
