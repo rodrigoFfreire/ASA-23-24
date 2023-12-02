81eb97f675084d04aa4794655ff879b4dc7a3c2c
rodrigoFfreire <rodrigofreitasfreire@gmail.com>
2023-12-02 02:23:02 +0000

    fix: forgot to add namespace

--- Project1/solver.h
+++ Project1/solver.h
@@ -1,13 +1,11 @@
 #ifndef __SOLVER_H__
 #define __SOLVER_H__
 
-#include <vector>
+uint computeComboX(std::vector<uint> &matrix, uint x, uint line);
 
-uint computeComboX(vector<uint> &matrix, uint x, uint line);
+uint computeComboY(std::vector<uint> &matrix, uint y, uint column, uint matrixX);
 
-uint computeComboY(vector<uint> &matrix, uint y, uint column, uint matrixX);
-
-uint solveBestValue(vector<uint> &matrix, uint sheetX, uint sheetY,
+uint solveBestValue(std::vector<uint> &matrix, uint sheetX, uint sheetY,
                     uint sIndex);
 
 #endif
