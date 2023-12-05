bdad767dd3ec93c84b592ccb0ce16c336dae681a
rodrigoFfreire <rodrigofreitasfreire@gmail.com>
2023-12-05 00:06:25 +0000

    compacted source code

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,44 +1,72 @@
 #include <iostream>
 #include <stdlib.h>
 #include <vector>
-#include "solver.h"
 
 using namespace std;
 
+uint solveBestValue(vector<uint> &matrix, uint sheetX, uint sheetY,
+                    uint sIndex) {
+    uint matrixX = sheetX + 1;
+    uint x = sIndex % matrixX;
+    uint y = sIndex / matrixX;
+
+    while (y <= sheetY) {
+        uint line = y * matrixX;
+        while (x < sheetX) {
+            sIndex++;
+            x++;
+            uint bestValue = matrix[sIndex];
+            for (uint ix = 1; ix <= x >> 1; ix++)
+                bestValue = max(bestValue, matrix[line + ix] + matrix[line + x - ix]);
+
+            for (uint iy = 1; iy <= y >> 1; iy++)
+                bestValue = max(bestValue, matrix[matrixX * iy + x] + matrix[(y - iy) * matrixX + x]);
+
+            matrix[sIndex] = bestValue;
+            if (x > y && x <= sheetY)
+                matrix[x * matrixX + y] = bestValue;
+        }
+        x = y;
+        y++;
+        sIndex += y;
+    }
+
+    return matrix[sheetY * matrixX + sheetX];
+}
+
 
 int main() {
-    uint sheetX, sheetY, pieceAmount;
+    uint X, Y, N; // Sheet X, Sheet Y, Number of Pieces
     uint result = 0;
 
-    scanf("%u %u", &sheetX, &sheetY);
-    scanf("%u", &pieceAmount);
+    scanf("%u %u", &X, &Y);
+    scanf("%u", &N);
 
-    if (pieceAmount > 0) {
-        if (sheetY > sheetX)
-            swap(sheetX, sheetY);
+    if (N > 0) {
+        if (Y > X)
+            swap(X, Y);
 
         uint startIndex = 0xFFFFFFFF;
-        uint matrixX = sheetX + 1;
-        uint pieceX, pieceY, piecePrice;
+        uint matrixX = X + 1;
+        uint a, b, v; // piece X, piece Y, piece Price
 
-        vector<uint> valueMatrix(matrixX * (sheetY + 1), 0);
-        for (; pieceAmount > 0; pieceAmount--) {
-            scanf("%u %u %u", &pieceX, &pieceY, &piecePrice);
-            if (pieceY > pieceX)
-                swap(pieceX, pieceY);
+        vector<uint> table(matrixX * (Y + 1), 0);
+        for (; N > 0; N--) {
+            scanf("%u %u %u", &a, &b, &v);
+            if (b > a)
+                swap(a, b);
 
-            uint index = pieceY * matrixX + pieceX;
-            valueMatrix[index] = max(valueMatrix[index], piecePrice);
-            if (pieceX <= sheetY)
-                valueMatrix[pieceX * matrixX + pieceY] =
-                    max(valueMatrix[pieceX * matrixX + pieceY], piecePrice);
+            uint index = b * matrixX + a;
+            table[index] = max(table[index], v);
+            if (a <= Y)
+                table[a * matrixX + b] = max(table[a * matrixX + b], v);
             startIndex = min(startIndex, index);
         }
         startIndex++;
         if (startIndex % matrixX == 0)
-            startIndex += startIndex / (sheetY + 1);
+            startIndex += startIndex / (Y + 1);
 
-        result = solveBestValue(valueMatrix, sheetX, sheetY, startIndex - 1);
+        result = solveBestValue(table, X, Y, startIndex - 1);
     }
     cout << result << endl;
     return 0;
